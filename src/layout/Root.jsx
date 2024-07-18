115deb2057d3173385c2f7a90c411aec96f1d286
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-07-18 12:45:11 +0600

    init

---
+++ src/layout/Root.jsx
@@ -0,0 +1,11 @@
+import Home from "../pages/Home/Home";
+
+const Root = () => {
+    return (
+        <div className="font-poppins">
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
