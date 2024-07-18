f38a355b0d7e979bc3e29cb971dd9c2a5ccbeae3
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-07-18 22:04:11 +0600

    add: Book Now Section

---
+++ src/components/BookNow/BookNow.jsx
@@ -0,0 +1,69 @@
+import { FaRegClock } from "react-icons/fa6";
+import { MdDateRange } from "react-icons/md";
+
+const BookNow = () => {
+  return (
+    <section className="pt-[7.5rem] pb-[7.375rem] bg-dark text-white">
+      <div className="flex flex-col md:flex-row gap-8 max-w-[1440px] w-10/12 mx-auto">
+        <div className="md:max-w-[29%]">
+          <span className="inline-block size-2 rounded-full bg-primary-color mt-1"></span>
+          <p className="font-semibold leading-[2.125rem]">Looking for taxi?</p>
+          <h1 className="text-[3.75rem] tracking-[-0.04rem] leading-[4.5rem] font-bold">Make your booking</h1>
+          <p className="font-medium mt-5 md:mt-[2.25rem] leading-[2.125rem] text-[#B5B5B5]">Our taxis commit to make your trips unique by best answering your needs.</p>
+        </div>
+        <form className="flex-1 grid gap-5 grid-cols-2">
+          {/* name */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="text" placeholder="Name" name="name" className="bg-transparent w-full outline-none" required />
+          </div>
+
+          {/* email */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="email" placeholder="Email" name="email" className="bg-transparent w-full outline-none" required />
+          </div>
+
+          {/* passengers */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="number" placeholder="Passengers #" name="passengers" className="bg-transparent w-full outline-none" required />
+          </div>
+
+          {/* pick up address */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="text" placeholder="Pick up address" name="pickUpAddress" className="bg-transparent w-full outline-none" required />
+          </div>
+
+          {/* drop off address */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="text" placeholder="Drop off address" name="dropOffAddress" className="bg-transparent w-full outline-none" required />
+          </div>
+
+          {/* select date */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#cfcfcf] font-semibold rounded-full py-6 px-10">
+            <input type="text" placeholder="Select date" name="selectDate" className="bg-transparent w-full outline-none" required />
+            <MdDateRange className="text-xl text-primary-color" />
+          </div>
+
+          {/* select time */}
+          <div className="w-full h-fit mx-auto flex items-center gap-3 text-sm bg-[#242424] text-[#b5b5b5] font-semibold rounded-full py-6 px-10">
+            <select name="role" defaultValue="default" className="w-full bg-transparent mx-auto gap-3 text-base outline-none cursor-pointer">
+              <option disabled value="default">
+                Select Time
+              </option>
+              <option value="10AM-10.59AM">10AM-10.59AM</option>
+              <option value="12PM-12.59PM">12PM-12.59PM</option>
+              <option value="1PM-1.59PM">1PM-1.59PM</option>
+              <option value="2PM-2.59PM">2PM-2.59PM</option>
+            </select>
+            <FaRegClock className="text-xl text-primary-color" />
+          </div>
+
+          <button className="font-semibold bg-primary-color text-dark text-lg rounded-[2.4375rem] py-4 px-[3.6875rem] duration-[.4s] hover:bg-white active:scale-90">
+            Learn More
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default BookNow;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Banner/Navbar/Navbar";
+import BookNow from "../../components/BookNow/BookNow";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div className="bg-dark h-[12.9rem] sm:hidden"></div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <BookNow></BookNow>
       <div className="max-w-[1440px] w-10/12 mx-auto"></div>
     </div>
   );
